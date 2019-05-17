e83369922ecf3275c8a97570f1b6e05e9c9b6f0d
Jonatas <jonatastdb_jp@hotmail.com>
2019-05-16 22:01:52 -0300

    first structure
    create components, modules, containers, theme

---
+++ src/app/containers/wprogress/wprogress.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { WprogressComponent } from './wprogress.component';
+
+@NgModule({
+    declarations: [
+        WprogressComponent
+    ],
+    imports: [],
+    exports: [
+        WprogressComponent
+    ]
+})
+export class WprogressModule{
+
+}
