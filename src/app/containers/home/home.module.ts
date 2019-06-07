64eaa07931542386a04d7e17960005ec5db9cf2a
Jonatas <jonatastdb_jp@hotmail.com>
2019-06-07 00:24:58 -0300

    home component --WIP

--- src/app/containers/home/home.module.ts
+++ src/app/containers/home/home.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from 'src/app/containers/home/home.component'
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HeaderModule } from 'components/header/header.module';
+import { FormModule } from 'modules/form/form.module';
 @NgModule({
   imports: [
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    FormModule
   ],
   declarations: [
     HomeComponent
