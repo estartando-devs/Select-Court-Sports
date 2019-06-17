24a181408da83740af9d7cfc395e5a84ca4f7ce6
Breno Nunes <brenontavares@gmail.com>
2019-06-17 00:02:26 -0300

    Modal container --WIP

--- src/app/containers/login/login.module.ts
+++ src/app/containers/login/login.module.ts
@@ -5,11 +5,13 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormModule } from '../../modules/form/form.module';
 import { RegisterComponent } from './register/register.component';
+import { ModalComponent } from 'src/app/shared/components/modal/modal.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ModalComponent
   ],
   imports: [
     CommonModule,
