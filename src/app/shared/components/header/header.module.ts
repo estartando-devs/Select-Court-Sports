5721f8ea45aa1838fa7b1d6b49e482d17686c336
Alan Araujo <alan.araujo@bennu.tv>
2019-06-25 02:02:55 -0300

    Schedule service - WIP

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header.component';
+import { ScheduleComponent } from 'src/app/containers/schedule/schedule.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    ScheduleComponent
   ],
   imports: [CommonModule],
   exports: [HeaderComponent]
