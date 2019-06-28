7e85a04a97adc0671aed8c2ed59726ee06332f05
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2019-06-28 00:24:42 -0300

    select month --wip

--- src/app/containers/schedule/select-month/select-month.component.ts
+++ src/app/containers/schedule/select-month/select-month.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/shared/components/modal/modal.service';
 
 @Component({
   selector: 'app-select-month',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectMonthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: ModalService) { }
 
   ngOnInit() {
   }
 
+  toggleModal(){
+    this.modalService.toggleModal();
+  } 
+
+  public months = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f"
+  ]
 }
