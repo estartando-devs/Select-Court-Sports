00f88363b05463605b3d5ee2ed2e1900db39bddb
Jonatas <jonatastdb_jp@hotmail.com>
2019-07-04 21:26:55 -0300

    create my-hours module.
    add my-hours component in my-hours module

---
+++ src/app/containers/my-hours/my-hours.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyHoursComponent } from './my-hours.component';
+
+@NgModule({
+  declarations: [MyHoursComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MyHoursModule { }
