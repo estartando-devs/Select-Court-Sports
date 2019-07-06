d387f34f0e36d9942ff3a32bf9e0d5e075b6c509
Alan Araujo <alan.araujo@bennu.tv>
2019-07-05 22:00:32 -0300

    Week day selected

--- src/app/containers/schedule/week-day/week-day.component.ts
+++ src/app/containers/schedule/week-day/week-day.component.ts
@@ -9,6 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class WeekDayComponent implements OnInit {
   public days = []
+  public activeIndex = 0
   @Output() setWeekDay = new EventEmitter<number>()
   @Output() setWeek = new EventEmitter<number>()
   @Input() $date : Observable<Date>
@@ -21,6 +22,7 @@ export class WeekDayComponent implements OnInit {
       let copyDate
       let firstDayOfWeek
       this.days = []
+      this.activeIndex = moment(date).weekday()
       for (let index = 0; index < 7; index++) {
         copyDate = date
         firstDayOfWeek = moment(copyDate).weekday(0)
