9490bde7e457be66e04fced1951830b00992e266
Jonatas <jonatastdb_jp@hotmail.com>
2019-07-07 14:27:48 -0300

    list-component with tbody scroll overflow

--- src/app/containers/schedule/list/list.component.ts
+++ src/app/containers/schedule/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LANG } from 'src/theme/pt';
+import { Schedule } from 'src/app/shared/models';
 
 @Component({
   selector: 'app-list',
@@ -9,9 +10,63 @@ import { LANG } from 'src/theme/pt';
 export class ListComponent implements OnInit {
 
   public lang = LANG
+
+  public generalHours = new Array<Schedule>()
+
   constructor() { }
 
   ngOnInit() {
+    this.generalHours = [
+      {
+        date:new Date("April 08, 2018 15:15:00"),
+        price: 100,
+        status: "Disponivel",
+      },
+      {
+        date:new Date("April 08, 2018 15:15:00"),
+        price: 100,
+        status: "Disponivel",
+      },
+      {
+        date: new Date("May 13, 2018 10:00:02"), 
+        price: 789, 
+        status:"Locado" 
+      },
+      {
+        date: new Date("May 13, 2018 10:00:02"), 
+        price: 789, 
+        status:"Locado" 
+      },
+      {
+        date: new Date("May 13, 2018 10:00:02"), 
+        price: 789, 
+        status:"Locado" 
+      },
+      {
+        date: new Date("May 13, 2018 10:00:02"), 
+        price: 789, 
+        status:"Locado" 
+      },
+      {
+        date: new Date("May 13, 2018 10:00:02"), 
+        price: 789, 
+        status:"Locado" 
+      },
+      {
+        date: new Date("May 13, 2018 10:00:02"), 
+        price: 789, 
+        status:"Locado" 
+      },
+      {
+        date: new Date("May 13, 2018 10:00:02"), 
+        price: 789, 
+        status:"Locado" 
+      },
+      {
+        date: new Date("July 10, 2018 12:00:40"), 
+        price: 100, 
+        status:"Disponivel" 
+      }
+    ]
   }
-
 }
